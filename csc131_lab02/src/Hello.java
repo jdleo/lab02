7e985318f5095b302605e33cfa80cef5f96476a5
sunshine <sunshine@Anuroctonus>
2018-10-26 03:27:58 +0000

    moved my line to line 9 to create conflict

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
+		
 		System.out.println("219906467 - This line was added by Dean Gramcko.");
 		System.out.println("218004801 - This line was added by John Lorenz Salva.");
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
