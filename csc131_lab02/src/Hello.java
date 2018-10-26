74904c3f13d579172fb88ab609ad69cb2f45729f
ehallcsus <edwardhall@csus.edu>
2018-10-25 21:03:35 -0700

    Added information for Edward

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -5,11 +5,11 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
+		System.out.println("215826846 - This line was added by Edward Hall.");
 		System.out.println("219906467 - This line was added by Dean Gramcko.");
 		System.out.println("218004801 - This line was added by John Lorenz Salva.");
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
 		System.out.println("216649967 - This line was added by John Leonardo <3");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
