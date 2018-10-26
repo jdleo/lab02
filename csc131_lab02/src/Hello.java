90870c4d2b343503da76b62d34e14c6af893eba9
shazimajaz1 <shazimajaz@192.168.0.12>
2018-10-25 21:49:56 -0700

    This was 6th change

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("218004801 - This line was added by John Lorenz Salva.");
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
 		System.out.println("216649967 - This line was added by John Leonardo <3");
-		System.out.println("219309442 - This line was added by Shazim Ajaz <41");
+		System.out.println("219309442 - This line was added by Shazim Ajaz <415");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
