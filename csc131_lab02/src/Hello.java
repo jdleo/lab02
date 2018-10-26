075e4b97c6542ab9ab8d04e4b3cc527ae81906fa
John Leonardo <jdleonardo5@icloud.com>
2018-10-26 16:58:33 -0700

    trying once more to force conflict

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -7,10 +7,10 @@ public class Hello {
 		System.out.println("215826846 - This line was added by Edward Hall.");
 		System.out.println("219906467 - This line was added by Dean Gramcko.");
 		System.out.println("218004801 - This line was added by John Lorenz Salva.");
-		System.out.println("211656628 - This line was added by Sam Hendryx. x");
+		System.out.println("211656628 - This line was added by Sam Hendryx.");
 		System.out.println("216649967 - This line was added by John Leonardo <3");
 		System.out.println("219309442 - This line was added by Shazim Ajaz <415");
-		System.out.println("219326290 - This line was added by Shouchern Lin."); // AKA Alan Lin
+		System.out.println("219326290 - This line was added by Shouchern Lin. x"); // AKA Alan Lin
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
