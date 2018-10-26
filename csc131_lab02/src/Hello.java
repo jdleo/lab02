ff598a3d905c7fc1a3dd7fbf8cae006b5f000eb0
shazimajaz1 <shazimajaz@192.168.0.12>
2018-10-25 21:08:31 -0700

    Second change made by Shazim

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("218004801 - This line was added by John Salva.");
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
 		System.out.println("216649967 - This line was added by John Leonardo <3");
-		System.out.println("219309442 - This line was added by Shazi Ajaz");
+		System.out.println("219309442 - This line was added by Shazim Ajaz");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
