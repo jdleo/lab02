29409e082ad552c0f7eac99a9c8999eb563fc504
John Leonardo <jdleonardo5@icloud.com>
2018-10-25 20:52:55 -0700

    im naughty

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219906467 - This line was added by Dean Gramcko.");
-		System.out.println("218004801 - This line was added by John Salva.");
+		System.out.println("218004801 - This line was added by John Salva. (hi john)");
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
 		System.out.println("216649967 - This line was added by John Leonardo <3");
 		System.out.println("SacStateID - This line was added by First name Last name.");
