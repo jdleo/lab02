ea798ee67fe4802d3bc92eec055a0a1f6d1e079b
jsjoh <jsjoh@Cloud-SalvaCom>
2018-10-25 20:40:09 -0700

    edited my name

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		
 		System.out.println("219906467 - This line was added by Dean Gramcko.");
-		System.out.println("218004801 - This line was added by John Lorenz Salva.");
+		System.out.println("218004801 - This line was added by John Salva.");
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
