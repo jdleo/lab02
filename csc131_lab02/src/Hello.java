38020bf8463f4786a2ff31fe2da903018942f950
leaptthroughtime <alanlinshouchern@gmail.com>
2018-10-25 21:46:45 -0700

    Alan's first commit

--- csc131_lab02/src/Hello.java
+++ csc131_lab02/src/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("211656628 - This line was added (by hand) by Sam Hendryx.");
 		System.out.println("216649967 - This line was added by John Leonardo <3");
 		System.out.println("219309442 - This line was added by Shazim Ajaz");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219326290 - This line was added by Shouchern Lin."); // Alan Lin
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
